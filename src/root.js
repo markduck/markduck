e05e36b1a45575c72b4bceccba6d90961bb04f3d
sergeyt <stodyshev@gmail.com>
2016-01-20 23:18:38 +0600

    init

--- src/root.js
+++ src/root.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+// import Sidebar from 'react-sidebar';
 import { Grid, Row, Col } from 'react-bootstrap';
 import Markdown from 'react-markdown2';
+import Ace from 'react-ace';
+
+import 'brace/mode/markdown';
+import 'brace/theme/github';
 
 export default class Root extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// TODO load last from local storage
+			content: '',
+		};
+	}
+
 	render() {
+		const onChange = value => {
+			this.setState({ content: value });
+		};
 		return (
 			<Grid className="app">
 				<Row>
-					<Col md={12}>
-						<Markdown source="*text*"/>
+					<Col md={6}>
+						<Ace mode="java" theme="github" value={this.state.content} onChange={onChange}/>
+					</Col>
+					<Col md={6}>
+						<Markdown source={this.state.content}/>
 					</Col>
 				</Row>
 			</Grid>
